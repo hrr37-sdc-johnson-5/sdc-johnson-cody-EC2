0295dd4229be94327b03184304c5d5b1883f35d3
Cody Smith <cms287@gmail.com>
2019-05-01 10:05:40 -0700

    updated db host

--- database/config.js
+++ database/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  host: 'ec2-54-219-167-96.us-west-1.compute.amazonaws.com',
+  host: 'ec2-18-144-22-89.us-west-1.compute.amazonaws.com',
   user: 'power_user',
   password: 'hrr37SDC-0412',
   database: 'bandland_comments',
